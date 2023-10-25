e9cdb820ed7f9ee73b044b4baeea4fe65cede35c
alikansoh <alikansoh.dev@gmail.com>
2023-10-25 16:57:41 +0300

    edit the comments

--- server.js
+++ server.js
@@ -2,8 +2,8 @@ import dotenv from 'dotenv';
 import mongoose from 'mongoose';
 import express from 'express';
 import companiesRoutes from './routes/companieRoute.js';
-// import feedbackRoutes from './routes/feedbacks.js';
-// import adminRoutes from './routes/adminRoute.js';
+import feedbackRoutes from './routes/feedbacks.js';
+import adminRoutes from './routes/adminRoute.js';
 import categoriesRoutes from './routes/categoriesRoute.js';
 import db from './configuration/db.js'; 
 const app = express();
@@ -21,9 +21,9 @@ app.use((req, res, next) => {
 
 // Routes
 app.use('/api/companies', companiesRoutes);
-// app.use('/api/feedbacks', feedbackRoutes)
+app.use('/api/feedbacks', feedbackRoutes)
 app.use('/api/categories', categoriesRoutes)
-// app.use('/api/admin',adminRoutes); 
+app.use('/api/admin',adminRoutes); 
 
 
 //listen to port 
