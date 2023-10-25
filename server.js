4036ab546188c3b177887c40825b556f347c3df1
Mohamadalyouness <mohamadalyouness7@gmail.com>
2023-10-25 12:18:30 +0300

    added feedback table

--- server.js
+++ server.js
@@ -2,8 +2,10 @@ import dotenv from 'dotenv';
 import mongoose from 'mongoose';
 import express from 'express';
 import companiestRoutes from './routes/companieRoute.js';
+import feedbackRoutes from './routes/feedbacks.js';
 import db from './configuration/db.js'; 
 const app = express();
+
 
 // Middleware
 app.use(express.json());
@@ -17,6 +19,7 @@ app.use((req, res, next) => {
 
 // Routes
 app.use('/api/companies', companiestRoutes);
+app.use('/api/feedbacks', feedbackRoutes)
 
 //listen to port 
 app.listen(process.env.PORT, () => {
