ffc75722747832ca1c143dc854dd388f53197d1c
Mohamadalyouness <mohamadalyouness7@gmail.com>
2023-11-13 14:21:59 +0200

    ew

--- server.js
+++ server.js
@@ -9,15 +9,14 @@ import designRoutes from './routes/designRoute.js';
 import db from './configuration/db.js'; 
 const app = express();
 const cors = require('cors');
-const corsOptions ={
-  origin:'*', 
-  credentials:true,          
-  optionSuccessStatus:200,
-}
+// const corsOptions ={
+//   origin:'*', 
+//   credentials:true,          
+// }
 
 // Middleware
 app.use(express.json());
-app.use(cors(corsOptions));
+app.use(cors());
 dotenv.config();
 
 
