41c2e3f14c497920381d02b6cac9795df6d63faf
malakhm <malakhmm22@gmail.com>
2023-12-18 12:45:20 +0200

    images half done

--- models/designModel.js
+++ models/designModel.js
@@ -1,23 +1,26 @@
-import sequelize from "../configuration/db.js";
-import { DataTypes } from "sequelize";
+import { DataTypes } from 'sequelize';
+import sequelize from '../configuration/db.js';
 
-import Company from "./companyModel.js";
-import Category from "./categoryModel.js";
+import Company from './companyModel.js';
+import Category from './categoryModel.js';
 
 const Design = sequelize.define(
-  "Design",
+  'Design',
   {
-    image: {
-      type: DataTypes.STRING,
+    images: {
+      type: DataTypes.ARRAY(DataTypes.STRING),
       allowNull: false,
     },
   },
-  { timestamps: true }
+  {
+    timestamps: true,
+  }
 );
-Company.hasMany(Design, { foreignKey: "CompanyId" });
-Design.belongsTo(Company, { foreignKey: "CompanyId" });
 
-Category.hasMany(Design, { foreignKey: "CategoryId", onDelete: "CASCADE" });
-Design.belongsTo(Category, { foreignKey: "CategoryId" });
+Company.hasMany(Design, { foreignKey: 'CompanyId' });
+Design.belongsTo(Company, { foreignKey: 'CompanyId' });
+
+Category.hasMany(Design, { foreignKey: 'CategoryId' });
+Design.belongsTo(Category, { foreignKey: 'CategoryId' });
 
 export default Design;
