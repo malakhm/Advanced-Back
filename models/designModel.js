d70edb311462efd75d449368025801698b8bd8fe
anitta-sleiman <sleiman3198@gmail.com>
2023-12-20 09:12:55 +0200

    removed cascade from design model

--- models/designModel.js
+++ models/designModel.js
@@ -19,10 +19,10 @@ const Design = sequelize.define(
   },
   { timestamps: true }
 );
-Company.hasMany(Design, { foreignKey: "CompanyId" ,onDelete: "CASCADE"});
+Company.hasMany(Design, { foreignKey: "CompanyId" });
 Design.belongsTo(Company, { foreignKey: "CompanyId" });
 
-Category.hasMany(Design, { foreignKey: "CategoryId", onDelete: "CASCADE" });
+Category.hasMany(Design, { foreignKey: "CategoryId" });
 Design.belongsTo(Category, { foreignKey: "CategoryId" });
 
 export default Design;
